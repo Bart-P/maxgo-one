4e0a9551d8b3be5ebd9f8032f6c48698055abfe4
bp <bartek.paraf@gmail.com>
2023-02-09 21:38:30 +0100

    enqueued fontawesome and custom-logo theme support

--- wp-content/themes/maxgo-one/functions.php
+++ wp-content/themes/maxgo-one/functions.php
@@ -3,6 +3,12 @@
 function load_css(): void {
 	wp_register_style('tailwind', get_template_directory_uri() . '/style.css');
 	wp_enqueue_style('tailwind');
+
+	wp_register_style('fontawesome', get_template_directory_uri() . '/node_modules/@fortawesome/fontawesome-free/css/fontawesome.css');
+	wp_enqueue_style('fontawesome');
+
+	wp_register_style('fa-solid', get_template_directory_uri() . '/node_modules/@fortawesome/fontawesome-free/css/solid.css');
+	wp_enqueue_style('fa-solid');
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
@@ -10,3 +16,5 @@ function load_js(): void {
 	wp_enqueue_script('jquery');
 }
 add_action('wp_enqueue_scripts', 'load_js');
+
+add_theme_support('custom-logo' );

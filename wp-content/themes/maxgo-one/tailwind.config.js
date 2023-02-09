348d9bd398c53813529de6aee69ea038dd8f20a6
bp <bartek.paraf@gmail.com>
2023-02-09 21:40:14 +0100

    custom colors and container sizes added

--- wp-content/themes/maxgo-one/tailwind.config.js
+++ wp-content/themes/maxgo-one/tailwind.config.js
@@ -1,7 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.php"],
   theme: {
+    colors: {
+      primary: '#F58612',
+      secondary: '#5B5B66',
+      light: '#F4F4F6',
+      dark: '#091E05',
+    },
+    container: {
+      center: true,
+      screens: {
+        'sm': '100%',
+        'md': '100%',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+    },
     extend: {},
   },
   plugins: [],

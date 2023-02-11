70bd3423a4589cfdef9bc98b9e5771176769db75
bp <bartek.paraf@gmail.com>
2023-02-11 13:34:23 +0100

    started footer design

--- wp-content/themes/maxgo-one/footer.php
+++ wp-content/themes/maxgo-one/footer.php
@@ -1,7 +1,20 @@
 <footer>
-    <p class="bg-green-200">
-        Das wird mal der footer
-    </p>
+    <div class="bg-dark text-light py-10">
+        <div class="flex justify-evenly container">
+            <div class="">
+                <p>maxgo GmbH</p>
+                <p>Hufeisenstr. 8</p>
+                <p>45525 Hattingen</p>
+                <p>Deutschland</p>
+                <p>Tel.: +49 2324 / 97703-01</p>
+                <p>Fax: +49 2324 / 97703-69</p>
+                <p>E-Mail: info@maxgo.eu</p>
+            </div>
+            <div class="">box 2</div>
+            <div class="">box 3</div>
+        </div>
+    </div>
+
 </footer>
 <?php wp_footer() ?>
 </body>

767600f76a008e3af94c085cca81f0b5111322b6
bp <bartek.paraf@gmail.com>
2023-02-11 20:51:37 +0100

    added comments for easier organization and added theme support form menus (navigation)

--- wp-content/themes/maxgo-one/functions.php
+++ wp-content/themes/maxgo-one/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// Load CSS
 function load_css(): void {
 	wp_register_style('tailwind', get_template_directory_uri() . '/style.css');
 	wp_enqueue_style('tailwind');
@@ -12,9 +13,20 @@ function load_css(): void {
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
+//Load Javascript (JQuery)
 function load_js(): void {
 	wp_enqueue_script('jquery');
 }
 add_action('wp_enqueue_scripts', 'load_js');
 
+// Theme Options
 add_theme_support('custom-logo' );
+add_theme_support('menus' );
+
+// Menus
+register_nav_menus(
+	array(
+		'top-navigation' => 'Top Menu Location',
+		'mobile-navigation' => 'Mobile Menu Location',
+	)
+);

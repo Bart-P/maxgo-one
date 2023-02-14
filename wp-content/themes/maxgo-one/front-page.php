c035aa2cdc23bb605d5cfa0b3449691b700e02b9
bp <bartek.paraf@gmail.com>
2023-02-14 18:26:22 +0100

    created banner placeholder

--- wp-content/themes/maxgo-one/front-page.php
+++ wp-content/themes/maxgo-one/front-page.php
@@ -1,5 +1,17 @@
 <?php get_header() ?>
 
+
+<div class="fp-banner h-[70vh] w-[100vw] z-10">
+	<div class="container w-[100%] h-[100%] flex justify-center items-center text-light z-50">
+		<div class="text-center">
+			<h1 class="align-end text-5xl font-bold mb-5">Einfach, Sicher Verbinden!</h1>
+			<p class="text-2xl">Ein schlauer, super geiler text kommt hier hin. Sollte aber definitiv nicht zu lang sein..</p>
+		</div>
+	</div>
+
+</div>
+
+
 <?php get_template_part('includes/section', 'content') ?>
 
 <?php get_footer() ?>

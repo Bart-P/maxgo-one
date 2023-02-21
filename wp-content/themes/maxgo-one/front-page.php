ac40c98bad13a6cb6c559c7307df7d5c6b2a3d25
bp <bartek.paraf@gmail.com>
2023-02-21 20:27:29 +0100

    bigger text and more vertical space

--- wp-content/themes/maxgo-one/front-page.php
+++ wp-content/themes/maxgo-one/front-page.php
@@ -49,16 +49,16 @@
 </div>
 
 <div class="py-10 bg-light w-[100vw]">
-    <section class="container py-10 flex gap-8">
+    <section class="container py-10 flex gap-10">
         <div class="basis-2/5 py-5 my-auto">
             <h3 class="text-3xl uppercase font-dark font-bold">SCHUTZKONTAKT</h3>
-            <p class="py-5 text-xl">
+            <p class="py-10 text-xl">
                 3-fach Mehrwegeverteiler,
                 IP44 Spritzwassergeschützt,
                 stabile Vollgummi Ausführung,
                 Schuko Ultra Pro Kupplung mit Spannungsanzeige.
             </p>
-            <button class="bg-dark text-light py-2 px-4 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
+            <button class="bg-dark text-light text-xl py-3 px-5 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
         </div>
         
         <div class="container basis-3/5 overflow-hidden">
@@ -70,7 +70,7 @@
 </div>
 
 <div class="py-10 w-[100vw]">
-    <section class="container py-10 flex gap-8">
+    <section class="container py-10 flex gap-10">
         <div class="basis-3/5 overflow-hidden shadow-md">
             <img src="http://localhost/maxgo/wp-content/uploads/2023/02/camping-grill-draussen.jpg"
                  class="object-cover object-center"
@@ -79,13 +79,13 @@
 
         <div class="basis-2/5 py-5 my-auto">
             <h3 class="text-3xl uppercase font-dark font-bold">Camping</h3>
-            <p class="py-5 text-xl">
+            <p class="py-10 text-xl">
                 Verlängerungen nach DIN VDE 0100 Teil 708,
                 2,- und 3-Wege Verteiler erhältlich,
                 Adapterkabel von CEE auf Schutzkontakt,
                 Kostenlose Beratung
             </p>
-            <button class="bg-dark text-light py-2 px-4 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
+            <button class="bg-dark text-light text-xl py-3 px-5 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
         </div>
     </section>
 </div>
@@ -93,15 +93,15 @@
 
 
 <div class="py-10 bg-light w-[100vw]">
-    <section class="container py-10 flex gap-8">
+    <section class="container py-10 flex gap-10">
         <div class="basis-2/5 py-5 my-auto">
             <h3 class="text-3xl uppercase font-dark font-bold">Solar</h3>
-            <p class="py-5 text-xl">
+            <p class="py-10 text-xl">
                 Ein toller Text über Solar.
                 Hier kommt noch irgendwas Schlaues hin,
                 ich weiß halt noch nicht genau was.
             </p>
-            <button class="bg-dark text-light py-2 px-4 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
+            <button class="bg-dark text-light text-xl py-3 px-5 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
         </div>
 
         <div class="basis-3/5 overflow-hidden">
@@ -113,7 +113,7 @@
 </div>
 
 <div class="py-10 w-[100vw]">
-    <section class="container py-10 flex gap-8">
+    <section class="container py-10 flex gap-10">
         <div class="basis-3/5 overflow-hidden shadow-md">
             <img src="http://localhost/maxgo/wp-content/uploads/2023/02/starkstrom-lebensgefahr.jpg"
                  class="object-cover object-center"
@@ -122,27 +122,27 @@
 
         <div class="basis-2/5 py-5 my-auto">
             <h3 class="text-3xl uppercase font-dark font-bold">Starkstrom</h3>
-            <p class="py-5 text-xl">
+            <p class="py-10 text-xl">
                 Langlebige und stabile Steckverbinder,
                 Schnittfeste PUR-Leitung H07BQ-F,
                 Große Auswahl an Stromverteilern,
                 Lieferung direkt auf die Baustelle.
             </p>
-            <button class="bg-dark text-light py-2 px-4 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
+            <button class="bg-dark text-light text-xl py-3 px-5 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
         </div>
     </section>
 </div>
 
 <div class="py-10 bg-light w-[100vw]">
-    <section class="container py-10 flex gap-8">
+    <section class="container py-10 flex gap-10">
         <div class="basis-2/5 py-5 my-auto">
             <h3 class="text-3xl uppercase font-dark font-bold">Event</h3>
-            <p class="py-5 text-xl">
+            <p class="py-10 text-xl">
                 Ein toller Text über Solar.
                 Hier kommt noch irgendwas Schlaues hin,
                 ich weiß halt noch nicht genau was.
             </p>
-            <button class="bg-dark text-light py-2 px-4 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
+            <button class="bg-dark text-light text-xl py-3 px-5 uppercase hover:text-primary hover:bg-secondary shadow-md">mehr</button>
         </div>
 
         <div class="basis-3/5 overflow-hidden">
@@ -153,8 +153,8 @@
     </section>
 </div>
 
-<div class="text-dark py-10 flex flex-col justify-center items-center gap-8">
-    <h4 class="text-3xl font-bold uppercase">Neugierig? Rufen Sie uns jetzt an!</h4>
+<div class="text-dark py-10 my-10 flex flex-col justify-center items-center gap-10">
+    <h4 class="text-3xl font-bold uppercase">Neugierig? Jetzt Kontaktieren!</h4>
     <button class="bg-primary text-2xl text-dark font-bold py-3 px-5 uppercase hover:text-light hover:bg-dark shadow-md">Kontakt</button>
 </div>
 

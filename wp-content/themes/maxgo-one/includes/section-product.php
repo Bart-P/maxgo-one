b45dbcb445f1a2c69da2c689878ce77f4003dc9d
bp <bartek.paraf@gmail.com>
2023-02-26 20:39:34 +0100

    revamped product page grid

--- wp-content/themes/maxgo-one/includes/section-product.php
+++ wp-content/themes/maxgo-one/includes/section-product.php
@@ -1,14 +1,14 @@
 <?php if (carbon_get_post_meta(get_queried_object_id(), 'crb_product_show')): ?>
-	<div class="fp-banner h-[80vh] w-[100vw] z-10 bg-light" style="background-image: url(<?php echo wp_get_attachment_image_url(carbon_get_post_meta(get_queried_object_id(), 'crb_banner_image'), 'big') ?>)">
-		<div class="container w-[100%] h-[100%]">
-            <div class="grid grid-cols-5 grid-rows-4 h-full">
-                <div class="border-2 border-dark col-span-5 text-center text-3xl">Das ist das Heading</div>
-                <div class="border-2 border-dark">bullet 1</div>
-                <div class="border-2 border-dark grid-start-2 col-span-3 row-span-2">bild</div>
-                <div class="border-2 border-dark">bullet 2</div>
-                <div class="border-2 border-dark">bullet 3</div>
-                <div class="border-2 border-dark">bullet 4</div>
-                <div class="border-2 border-dark col-span-5 text-center">bullet 5</div>
+	<div class="fp-banner h-[80vh] w-[100vw] z-10 bg-light">
+		<div class="container w-full h-full">
+            <div class="border-2 border-dark text-center text-3xl py-10">Das ist das Heading</div>
+            <div class="grid grid-cols-7 grid-rows-3 h-full mb-10">
+                <div class="border-2 border-dark row-start-1 row-span-2 col-start-3 col-span-3">bild</div>
+                <div class="border-2 border-dark col-start-1 col-span-2">bullet 1</div>
+                <div class="border-2 border-dark col-span-2">bullet 3</div>
+                <div class="border-2 border-dark col-start-1 col-span-2">bullet 2</div>
+                <div class="border-2 border-dark col-span-2">bullet 4</div>
+                <div class="border-2 border-dark col-start-3 col-span-3 text-center">bullet 5</div>
             </div>
 		</div>
 	</div>

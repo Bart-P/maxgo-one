7a5cbc04064d57d2f9b7c09f0e4f59561c69cd8a
bp <bartek.paraf@gmail.com>
2023-02-27 08:31:55 +0100

    added margin around content part

--- wp-content/themes/maxgo-one/single-products.php
+++ wp-content/themes/maxgo-one/single-products.php
@@ -2,6 +2,8 @@
 
 <?php get_template_part('includes/section', 'banner') ?>
 <?php get_template_part('includes/section', 'product') ?>
-<?php get_template_part('includes/section', 'content') ?>
+<div class="my-10">
+	<?php get_template_part('includes/section', 'content') ?>
+</div>
 
 <?php get_footer() ?>

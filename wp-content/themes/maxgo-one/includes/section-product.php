18c6db6c70158c7e00e05e741b3d9bdba4393194
bp <bartek.paraf@gmail.com>
2023-03-11 11:11:16 +0100

    added some padding in gird, so it does not look so cramped up.

--- wp-content/themes/maxgo-one/includes/section-product.php
+++ wp-content/themes/maxgo-one/includes/section-product.php
@@ -9,13 +9,13 @@
                     <?php echo carbon_get_post_meta(get_queried_object_id(), 'crb_product_description') ?>
                 </p>
             </div>
-            <div class="grid grid-cols-7 grid-rows-3 h-full mb-10 text-xl leading-loose">
+            <div class="grid grid-cols-7 grid-rows-3 h-full mb-10 text-xl">
                 <div class="text-center w-full h-full row-start-1 row-span-2 col-start-3 col-span-3">
                     <img src="<?php echo wp_get_attachment_image_url(carbon_get_post_meta(get_queried_object_id(), 'crb_product_image'), 'big') ?>"
                          class="object-center object-cover"
                          alt="">
                 </div>
-                <div class="text-center my-auto col-start-1 col-span-2 space-y-5 max-w-[300px]">
+                <div class="text-center my-auto p-4 col-start-1 col-span-2 space-y-5 max-w-[300px]">
                     <div class="text-primary">
                         <i class="fa-solid fa-plug-circle-bolt text-3xl"></i>
                     </div>
@@ -23,7 +23,7 @@
                         <?php echo carbon_get_post_meta(get_queried_object_id(), 'crb_product_bullet1') ?>
                     </p>
                 </div>
-                <div class="text-center my-auto col-span-2 space-y-5 max-w-[300px]">
+                <div class="text-center my-auto p-4 col-span-2 space-y-5 max-w-[300px]">
                     <div class="text-primary">
                         <i class="fa-solid fa-person-digging text-3xl"></i>
                     </div>
@@ -31,7 +31,7 @@
                         <?php echo carbon_get_post_meta(get_queried_object_id(), 'crb_product_bullet3') ?>
                     </p>
                 </div>
-                <div class="text-center my-auto col-span-2 space-y-5 max-w-[300px]">
+                <div class="text-center my-auto p-4 col-span-2 space-y-5 max-w-[300px]">
                     <div class="text-primary">
                         <i class="fa-solid fa-shield-halved text-3xl"></i>
                     </div>
@@ -39,7 +39,7 @@
                         <?php echo carbon_get_post_meta(get_queried_object_id(), 'crb_product_bullet2') ?>
                     </p>
                 </div>
-                <div class="text-center my-auto col-span-2 space-y-5 max-w-[300px]">
+                <div class="text-center my-auto p-4 col-span-2 space-y-5 max-w-[300px]">
                     <div class="text-primary">
                         <i class="fa-solid fa-ruler text-3xl"></i>
                     </div>
@@ -47,7 +47,7 @@
                         <?php echo carbon_get_post_meta(get_queried_object_id(), 'crb_product_bullet4') ?>
                     </p>
                 </div>
-                <div class="text-center my-auto mx-auto col-start-3 col-span-3 text-center space-y-5 max-w-[300px]">
+                <div class="text-center my-auto p-4 mx-auto col-start-3 col-span-3 text-center space-y-5 max-w-[300px]">
                         <div class="text-primary">
                             <i class="fa-solid fa-hammer text-3xl"></i>
                         </div>

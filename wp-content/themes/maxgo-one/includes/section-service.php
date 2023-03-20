c25f8b8cadcdf8036af5943f63ae02a30e1e6ccb
bp <bartek.paraf@gmail.com>
2023-03-20 08:18:56 +0100

    service section created with filler

---
+++ wp-content/themes/maxgo-one/includes/section-service.php
@@ -0,0 +1,14 @@
+<div class="container flex h-[50vh] mt-10">
+	<div class="basis-1/2 bg-secondary grid grid-cols-2 grid-rows-2 gap-2 p-2">
+		<div class="border border-2 border-dark">1</div>
+		<div class="border border-2 border-dark">2</div>
+		<div class="border border-2 border-dark">3</div>
+		<div class="border border-2 border-dark">4</div>
+	</div>
+	<div class="basis-1/2 bg-primary p-2 ">
+		<div class="border border-2 border-dark h-full">
+			<h3 class="text-3xl font-bold">Testing stuff out</h3>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aliquid aspernatur dicta dolore, harum ipsa libero magnam magni molestiae perspiciatis quas, rem similique sunt, tempora. Ad dolores necessitatibus sed.
+		</div>
+	</div>
+</div>

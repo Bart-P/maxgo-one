0380c02e4b9c51c09b43c12da70c7a8f0ebfb6d2
bp <bartek.paraf@gmail.com>
2023-03-22 07:15:31 +0100

    created cards for 4 Bullets header in Services

--- wp-content/themes/maxgo-one/includes/section-service.php
+++ wp-content/themes/maxgo-one/includes/section-service.php
@@ -1,12 +1,36 @@
-<div class="container flex h-[50vh] mt-10">
-	<div class="basis-1/2 bg-secondary grid grid-cols-2 grid-rows-2 gap-2 p-2">
-		<div class="border border-2 border-dark">1</div>
-		<div class="border border-2 border-dark">2</div>
-		<div class="border border-2 border-dark">3</div>
-		<div class="border border-2 border-dark">4</div>
+<div class="container flex h-[70vh] mt-10 gap-6">
+	<div class="basis-1/2 grid grid-cols-2 grid-rows-2 gap-6 p-2">
+		<div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+            <div class="bg-primary rounded-full">
+                <i class="fa-solid fa-robot text-white p-2 text-xl"></i>
+            </div>
+            <h3 class="text-xl font-bold">Automatisierter Prozess</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
+        </div>
+        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+            <div class="bg-primary rounded-full">
+                <i class="fa-solid fa-warehouse text-white p-2 text-xl"></i>
+            </div>
+            <h3 class="text-xl font-bold">Viele Kilometer auf Lager</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
+        </div>
+        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+            <div class="bg-primary rounded-full">
+                <i class="fa-solid fa-gauge-high text-white p-2 text-xl"></i>
+            </div>
+            <h3 class="text-xl font-bold">Schnelle Reaktionszeit</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
+        </div>
+        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+            <div class="bg-primary rounded-full">
+                <i class="fa-solid fa-ruler-combined text-white p-2 text-xl"></i>
+            </div>
+            <h3 class="text-xl font-bold">Auf Maß Zugeschnitten</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
+        </div>
 	</div>
 	<div class="basis-1/2 bg-primary p-2 ">
-		<div class="border border-2 border-dark h-full">
+		<div class="">
 			<h3 class="text-3xl font-bold">Testing stuff out</h3>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aliquid aspernatur dicta dolore, harum ipsa libero magnam magni molestiae perspiciatis quas, rem similique sunt, tempora. Ad dolores necessitatibus sed.
 		</div>

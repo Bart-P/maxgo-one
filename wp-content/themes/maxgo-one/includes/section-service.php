46534c4fbb38b1f250d7217a5a0f311bf2320ce0
bp <bartek.paraf@gmail.com>
2023-03-22 07:24:08 +0100

    finished up service header style

--- wp-content/themes/maxgo-one/includes/section-service.php
+++ wp-content/themes/maxgo-one/includes/section-service.php
@@ -1,38 +1,39 @@
 <div class="container flex h-[70vh] mt-10 gap-6">
+    <div class="basis-1/2 p-4 space-y-8 my-auto">
+        <h1 class="text-4xl font-bold">Kabel und Verlängerungen zu Großhandelspreisen</h1>
+        <p class="text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aliquid aspernatur dicta dolore, harum ipsa libero magnam magni molestiae perspiciatis quas, rem similique sunt, tempora. Ad dolores necessitatibus sed.
+        </p>
+        <button class="btn btn-big btn-primary">Fragen Sie Uns An!</button>
+    </div>
 	<div class="basis-1/2 grid grid-cols-2 grid-rows-2 gap-6 p-2">
-		<div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+		<div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
             <div class="bg-primary rounded-full">
                 <i class="fa-solid fa-robot text-white p-2 text-xl"></i>
             </div>
             <h3 class="text-xl font-bold">Automatisierter Prozess</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
         </div>
-        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
             <div class="bg-primary rounded-full">
                 <i class="fa-solid fa-warehouse text-white p-2 text-xl"></i>
             </div>
             <h3 class="text-xl font-bold">Viele Kilometer auf Lager</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
         </div>
-        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
             <div class="bg-primary rounded-full">
                 <i class="fa-solid fa-gauge-high text-white p-2 text-xl"></i>
             </div>
             <h3 class="text-xl font-bold">Schnelle Reaktionszeit</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
         </div>
-        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md">
+        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
             <div class="bg-primary rounded-full">
                 <i class="fa-solid fa-ruler-combined text-white p-2 text-xl"></i>
             </div>
             <h3 class="text-xl font-bold">Auf Maß Zugeschnitten</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
         </div>
-	</div>
-	<div class="basis-1/2 bg-primary p-2 ">
-		<div class="">
-			<h3 class="text-3xl font-bold">Testing stuff out</h3>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aliquid aspernatur dicta dolore, harum ipsa libero magnam magni molestiae perspiciatis quas, rem similique sunt, tempora. Ad dolores necessitatibus sed.
-		</div>
 	</div>
 </div>

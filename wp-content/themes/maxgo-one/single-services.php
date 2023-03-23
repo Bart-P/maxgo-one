f34d89914e243c4eb43e757eb744a92ebddfb7fd
bp <bartek.paraf@gmail.com>
2023-03-23 07:25:56 +0100

    added some box styling to main service content

--- wp-content/themes/maxgo-one/single-services.php
+++ wp-content/themes/maxgo-one/single-services.php
@@ -3,8 +3,8 @@
 <?php get_template_part('includes/section', 'banner') ?>
 <?php get_template_part('includes/section', 'service') ?>
 
-	<div class="my-10 py-10 flex justify-between gap-8 container">
-        <div class="page-content">
+	<div class="my-10 flex justify-between gap-8 container">
+        <div class="page-content bg-light p-6 rounded-md shadow-md">
             <h2 class="text-2xl uppercase font-bold mb-8">
                 <?php the_title() ?>
             </h2>

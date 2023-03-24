67a59983f3c6e2bd059440bdb02e56347b9a6315
bp <bartek.paraf@gmail.com>
2023-03-24 07:58:23 +0100

    icons different icons are now shown for differen fields

--- wp-content/themes/maxgo-one/includes/section-service.php
+++ wp-content/themes/maxgo-one/includes/section-service.php
@@ -7,37 +7,23 @@
         <button class="btn btn-big btn-primary">Fragen Sie uns An!</button>
     </div>
 	<div class="basis-1/2 grid grid-cols-2 grid-rows-2 gap-6 p-2">
-        <?php foreach (carbon_get_post_meta(get_queried_object_id(), 'crb_service_bullets') as $bullet): ?>
+        <?php foreach (carbon_get_post_meta(get_queried_object_id(), 'crb_service_bullets') as $index=>$bullet): ?>
 
 		<div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
             <div class="bg-primary rounded-full">
+                <?php if ($index === 0): ?>
                 <i class="fa-solid fa-robot text-white p-2 text-xl"></i>
+                <?php elseif ($index === 1): ?>
+                <i class="fa-solid fa-warehouse text-white p-2 text-xl"></i>
+                <?php elseif ($index === 2): ?>
+                <i class="fa-solid fa-gauge-high text-white p-2 text-xl"></i>
+                <?php elseif ($index === 3): ?>
+                <i class="fa-solid fa-ruler-combined text-white p-2 text-xl"></i>
+                <?php endif; ?>
             </div>
             <h3 class="text-xl font-bold"><?php echo $bullet['crb_service_bullet_header'] ?></h3>
             <p><?php echo $bullet['crb_service_bullet_description'] ?></p>
         </div>
         <?php endforeach; ?>
-
-<!--        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
-            <div class="bg-primary rounded-full">
-                <i class="fa-solid fa-warehouse text-white p-2 text-xl"></i>
-            </div>
-            <h3 class="text-xl font-bold">Viele Kilometer auf Lager</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
-        </div>
-        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
-            <div class="bg-primary rounded-full">
-                <i class="fa-solid fa-gauge-high text-white p-2 text-xl"></i>
-            </div>
-            <h3 class="text-xl font-bold">Schnelle Reaktionszeit</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
-        </div>
-        <div class="flex flex-col gap-3 justify-center items-center text-center bg-light rounded-md shadow-md p-4">
-            <div class="bg-primary rounded-full">
-                <i class="fa-solid fa-ruler-combined text-white p-2 text-xl"></i>
-            </div>
-            <h3 class="text-xl font-bold">Auf Maß Zugeschnitten</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga inventore minima nobis</p>
-        </div>
--->	</div>
+    </div>
 </div>
